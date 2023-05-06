d809ed749366b87dcb07b2c298bb9d254e0c1693
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2023-05-06 20:28:52 +0300

    end of weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ImSearch } from "react-icons/im";
+import { WiCelsius } from "react-icons/wi";
+import "./App.css";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+  return(
+    <div>      
+      <script src="./script.js" defer></script>
+      <div className="card">
+        <div className="search">
+          <input type="text" className="search-bar"></input>
+          <button ><ImSearch  ></ImSearch></button>
+        </div>
+      
+      <div className="weather">
+        <h2 className="city"> Weather in denver</h2>
+        <div class="temp">51<WiCelsius></WiCelsius></div>
+        <img className="icon"></img>
+        <div className="description"></div>
+        <div className="humidity"> Humidity 60%</div>
+        <div className="wind"> Wind 10km/h</div>
+      </div>
+      </div>
+    </div>
+  )
+}
 export default App;
